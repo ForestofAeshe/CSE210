15dd5afc542e415b7f88e5a8f3c3dddd47db857a
ForestofAeshe <msawpetersen@gmail.com>
2025-04-11 21:49:52 -0600

    week 6 prepare activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,27 @@
 using System;
-
+using Shape;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape.Shape> shapes = new List<Shape.Shape>();
+
+        Square shape1 = new Square("Blue", 4);
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new Rectangle("Red", 4, 5);
+        shapes.Add(shape2);
+
+        Circle shape3 = new Circle("Green", 4);
+        shapes.Add(shape3);
+
+        foreach (Shape.Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shap has an area of {area}");
+        }
     }
 }
---
+++ week06/Shapes/Square.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Shape
+{
+    public class Square : Shape
+    {
+        public double _side;
+
+        public Square(string color, double side) : base(color) 
+        {
+            _side = side;
+        }
+
+        public override double GetArea()
+        {
+            return _side * _side;
+        }
+
+
+    }
+}
